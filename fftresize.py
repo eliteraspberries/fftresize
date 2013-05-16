da373f50ee9183326ba57ebbf1f26c73885e5c05
Mansour Moufid <mansourmoufid@gmail.com>
2013-05-16 01:18:37 -0400

    Fix some whitespace style issues.

--- fftresize.py
+++ fftresize.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python2
 # Copyright 2013, Mansour Moufid <mansourmoufid@gmail.com>
 
+
 from matplotlib import image, pyplot
 from numpy import append, array as _array, real, zeros as _zeros
 from numpy.fft import fft2, ifft2, fftshift, ifftshift
@@ -13,6 +14,7 @@ from os.path import basename, exists, splitext
 from random import randint
 from sys import argv as _argv, exit as _exit, stderr as _stderr
 
+
 def _zeropad2(x, shape):
     m, n = x.shape
     p, q = shape
@@ -20,13 +22,14 @@ def _zeropad2(x, shape):
     assert q > n
     tb = _zeros(((p - m) / 2, n))
     lr = _zeros((p, (q - n) / 2))
-    x = append(tb, x, axis = 0)
-    x = append(x, tb, axis = 0)
-    x = append(lr, x, axis = 1)
-    x = append(x, lr, axis = 1)
+    x = append(tb, x, axis=0)
+    x = append(x, tb, axis=0)
+    x = append(lr, x, axis=1)
+    x = append(x, lr, axis=1)
     return x
 
-def resize(filename, factor = 1.5):
+
+def resize(filename, factor=1.5):
     img = image.imread(filename)
     fft = fft2(img)
     fft = fftshift(fft)
@@ -37,21 +40,24 @@ def resize(filename, factor = 1.5):
     ifft = real(ifft)
     return ifft
 
+
 def _save(img, file):
     while True:
         newfile = splitext(file)[0] + '-'
-        newfile = newfile + str(randint(0,1000)) + '.png'
+        newfile = newfile + str(randint(0, 1000)) + '.png'
         if not exists(newfile):
             break
     swap = lambda (x, y): (y, x)
-    fig = pyplot.figure(figsize = swap(img.shape), dpi = 1)
-    fig.figimage(img, cmap = pyplot.cm.gray)
-    pyplot.savefig(newfile, dpi = 1)
+    fig = pyplot.figure(figsize=swap(img.shape), dpi=1)
+    fig.figimage(img, cmap=pyplot.cm.gray)
+    pyplot.savefig(newfile, dpi=1)
     return newfile
+
 
 def _fail(code):
     print>>_stderr, 'usage: fftresize.py <file> [alpha]'
     _exit(code)
+
 
 if '__main__' in __name__:
     if len(_argv) == 2:
