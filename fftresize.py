2a2c7b96c2ea2da8f8fe38d27dce05a8a6b52e38
Mansour Moufid <mansourmoufid@gmail.com>
2013-05-16 01:15:59 -0400

    Add a leading underscore to non-public methods and variables.

--- fftresize.py
+++ fftresize.py
@@ -2,23 +2,24 @@
 # Copyright 2013, Mansour Moufid <mansourmoufid@gmail.com>
 
 from matplotlib import image, pyplot
-from numpy import append, array, real, zeros
+from numpy import append, array as _array, real, zeros as _zeros
 from numpy.fft import fft2, ifft2, fftshift, ifftshift
 try:
-    from os import EX_NOINPUT, EX_USAGE
+    from os import EX_NOINPUT as _EX_NOINPUT
+    from os import EX_USAGE as _EX_USAGE
 except ImportError:
-    EX_NOINPUT, EX_USAGE = 1, 2
+    _EX_NOINPUT, _EX_USAGE = 1, 2
 from os.path import basename, exists, splitext
 from random import randint
-from sys import argv, exit, stderr
+from sys import argv as _argv, exit as _exit, stderr as _stderr
 
-def zeropad2(x, shape):
+def _zeropad2(x, shape):
     m, n = x.shape
     p, q = shape
     assert p > m
     assert q > n
-    tb = zeros(((p - m) / 2, n))
-    lr = zeros((p, (q - n) / 2))
+    tb = _zeros(((p - m) / 2, n))
+    lr = _zeros((p, (q - n) / 2))
     x = append(tb, x, axis = 0)
     x = append(x, tb, axis = 0)
     x = append(lr, x, axis = 1)
@@ -30,13 +31,13 @@ def resize(filename, factor = 1.5):
     fft = fft2(img)
     fft = fftshift(fft)
     reshape = lambda (x, y), a: (int(a * x), int(a * y))
-    fft = zeropad2(fft, reshape(img.shape, factor))
+    fft = _zeropad2(fft, reshape(img.shape, factor))
     ifft = ifftshift(fft)
     ifft = ifft2(ifft)
     ifft = real(ifft)
     return ifft
 
-def save(img, file):
+def _save(img, file):
     while True:
         newfile = splitext(file)[0] + '-'
         newfile = newfile + str(randint(0,1000)) + '.png'
@@ -48,27 +49,27 @@ def save(img, file):
     pyplot.savefig(newfile, dpi = 1)
     return newfile
 
-def fail(code):
-    print>>stderr, 'usage: fftresize.py <file> [alpha]'
-    exit(code)
+def _fail(code):
+    print>>_stderr, 'usage: fftresize.py <file> [alpha]'
+    _exit(code)
 
 if '__main__' in __name__:
-    if len(argv) == 2:
-        file = argv[1]
+    if len(_argv) == 2:
+        file = _argv[1]
         if not exists(file):
-            exit(EX_NOINPUT)
-    elif len(argv) == 3:
-        file = argv[1]
+            _exit(_EX_NOINPUT)
+    elif len(_argv) == 3:
+        file = _argv[1]
         if not exists(file):
-            exit(EX_NOINPUT)
+            _exit(_EX_NOINPUT)
         try:
-            factor = float(argv[2])
+            factor = float(_argv[2])
         except ValueError:
-            fail(EX_USAGE)
+            _fail(_EX_USAGE)
     else:
-        fail(EX_USAGE)
+        _fail(_EX_USAGE)
     try:
         x = resize(file, factor)
     except NameError:
         x = resize(file)
-    print save(x, file)
+    print _save(x, file)
