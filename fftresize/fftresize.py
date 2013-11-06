d142dc841c6a5c4fc412409f62dd671322679b7d
Mansour Moufid <mansourmoufid@gmail.com>
2013-11-06 17:15:00 -0500

    Bump to version 0.3.

--- fftresize/fftresize.py
+++ fftresize/fftresize.py
@@ -13,7 +13,7 @@ from . import imutils
 __author__ = 'Mansour Moufid'
 __copyright__ = 'Copyright 2013, Mansour Moufid'
 __license__ = 'ISC'
-__version__ = '0.2'
+__version__ = '0.3'
 __email__ = 'mansourmoufid@gmail.com'
 __status__ = 'Development'
 
