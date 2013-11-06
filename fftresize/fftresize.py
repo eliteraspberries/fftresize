ab478e5f727e7600d3bd5a2c3cdd7bfcfbffe1f9
Mansour Moufid <mansourmoufid@gmail.com>
2013-11-06 16:55:07 -0500

    Separate the script from the package.

--- fftresize/fftresize.py
+++ fftresize/fftresize.py
@@ -9,13 +9,6 @@ FFTresize resizes images using zero-padding in the frequency domain.
 from fftinterp import interp2
 import imutils
 from numpy import zeros as _zeros
-try:
-    from os import EX_NOINPUT as _EX_NOINPUT
-    from os import EX_USAGE as _EX_USAGE
-except ImportError:
-    _EX_NOINPUT, _EX_USAGE = 1, 2
-from os.path import exists
-from sys import argv as _argv, exit as _exit, stderr as _stderr
 
 
 __author__ = 'Mansour Moufid'
@@ -48,28 +41,5 @@ def resize(filename, factor=1.5):
     return imutils.save(new, filename)
 
 
-def _fail(code):
-    print>>_stderr, 'usage: fftresize.py <file> [alpha]'
-    _exit(code)
-
-
 if '__main__' in __name__:
-    if len(_argv) == 2:
-        file = _argv[1]
-        if not exists(file):
-            _exit(_EX_NOINPUT)
-    elif len(_argv) == 3:
-        file = _argv[1]
-        if not exists(file):
-            _exit(_EX_NOINPUT)
-        try:
-            factor = float(_argv[2])
-        except ValueError:
-            _fail(_EX_USAGE)
-    else:
-        _fail(_EX_USAGE)
-    try:
-        x = resize(file, factor)
-    except NameError:
-        x = resize(file)
-    print x
+    pass
