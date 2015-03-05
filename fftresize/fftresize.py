90169c80e02c9a57a9ccc46545300f2c81ab14c7
Mansour Moufid <mansourmoufid@gmail.com>
2015-03-05 17:08:45 -0500

    Update the copyright statement.

--- fftresize/fftresize.py
+++ fftresize/fftresize.py
@@ -11,7 +11,7 @@ from . import fftinterp
 
 
 __author__ = 'Mansour Moufid'
-__copyright__ = 'Copyright 2013, 2014, Mansour Moufid'
+__copyright__ = 'Copyright 2013-2015, Mansour Moufid'
 __license__ = 'ISC'
 __version__ = '0.5'
 __email__ = 'mansourmoufid@gmail.com'
