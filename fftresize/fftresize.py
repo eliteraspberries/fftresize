2eebffb2fef46b1a7c3d623df48619f766b2f1b1
Mansour Moufid <mansourmoufid@gmail.com>
2015-08-30 12:44:42 -0400

    Move the resize function to the script.

--- fftresize/fftresize.py
+++ fftresize/fftresize.py
@@ -4,8 +4,6 @@
 domain.
 '''
 
-from avena import image, interp
-
 
 __author__ = 'Mansour Moufid'
 __copyright__ = 'Copyright 2013-2015, Mansour Moufid'
@@ -13,19 +11,6 @@ __license__ = 'ISC'
 __version__ = '0.7'
 __email__ = 'mansourmoufid@gmail.com'
 __status__ = 'Development'
-
-
-_EXT = '.png'
-
-
-def resize(filename, factor=1.5):
-    '''Resize an image by zero-padding in the frequency domain.
-
-    Return the filename of the resized image.
-    '''
-    img = image.read(filename)
-    new = interp.interp2(img, factor)
-    return image.save(new, filename, random=True, ext=_EXT, normalize=True)
 
 
 if '__main__' in __name__:
